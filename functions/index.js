599075aa7fd202ecdd5955b556daafe8af4b2dd9
David-Z-1 <david.zheng20@imperial.ac.uk>
2022-06-19 21:17:02 +0100

    data_transmit_basic

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,12 @@ const cors = require("cors");
 const { firebaseConfig } = require("firebase-functions");
 const admin = require("firebase-admin")
 
-const app = express();
-app.use(cors());
+const rover_app = express();
+rover_app.use(cors());
+const alien_app = express();
+alien_app.use(cors());
+const new_alien_app = express();
+new_alien_app.use(cors());
 
 var serviceAccount = require('./admin.json');
 admin.initializeApp({
@@ -18,7 +22,7 @@ var db = admin.database();
 const ref_rover = db.ref("/rover_loc");
 const ref_alien = db.ref("/alien_loc");
 
-app.get('/', async (req,res)=>{
+rover_app.get('/', async (req,res)=>{
     console.log('rover site')
     ref_rover
     .once("value")
@@ -31,23 +35,7 @@ app.get('/', async (req,res)=>{
     });
 })
 
-exports.rover_1 = functions.https.onRequest(app);
-
-
-exports.rover = functions.https.onRequest((req, res) => {
-    console.log('rover site')
-    ref_rover
-    .once("value")
-    .then((snapshot) => {
-        var data = snapshot.val();  //Data is in JSON format.
-        new_data=JSON.stringify(data[data.length-1]);
-        const obj = JSON.parse(new_data);
-        console.log(obj); 
-        res.send([obj['x-axis'],obj['y-axis']]);
-    });
-});
-
-exports.alien = functions.https.onRequest((req, res) => {
+alien_app.get('/', async (req,res)=>{
     console.log('alien site')
     ref_alien
     .once("value")
@@ -63,7 +51,59 @@ exports.alien = functions.https.onRequest((req, res) => {
         }
         res.send(data_alien_final);
     });
-});
+})
+
+new_alien_app.get('/', async (req,res)=>{
+    console.log('alien site')
+    ref_alien
+    .once("value")
+    .then((snapshot) => {
+        var data_alien = snapshot.val();  //Data is in JSON format.
+        console.log("data_fetched", data_alien)
+        new_data=JSON.stringify(data_alien[data_alien.length-1]);
+        console.log("newest_data", new_data)
+        const obj = JSON.parse(new_data);
+        res.send(obj);
+    });
+})
+
+
+exports.rover_1 = functions.https.onRequest(rover_app);
+exports.alien_1 = functions.https.onRequest(alien_app);
+exports.new_alien_app = functions.https.onRequest(new_alien_app);
+
+
+
+// exports.rover = functions.https.onRequest((req, res) => {
+//     console.log('rover site')
+//     ref_rover
+//     .once("value")
+//     .then((snapshot) => {
+//         var data = snapshot.val();  //Data is in JSON format.
+//         new_data=JSON.stringify(data[data.length-1]);
+//         const obj = JSON.parse(new_data);
+//         console.log(obj); 
+//         res.send([obj['x-axis'],obj['y-axis']]);
+//     });
+// });
+
+// exports.alien = functions.https.onRequest((req, res) => {
+//     console.log('alien site')
+//     ref_alien
+//     .once("value")
+//     .then((snapshot) => {
+//         var data_alien = snapshot.val();  //Data is in JSON format.
+//         // new_data=JSON.stringify(data[data.length-1]);
+//         data_alien_final = new Array;
+//         for (let i = 0; i < data_alien.length; i++) {
+//           //console.log("json.parse: ",data_alien[i]);
+//           const tmp_data = JSON.stringify(data_alien[i]); 
+//           const obj = JSON.parse(tmp_data);
+//           data_alien_final.push(obj);
+//         }
+//         res.send(data_alien_final);
+//     });
+// });
 
 
 // var data_alien = snapshot.val();  //Data is in JSON format.
